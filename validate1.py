c4045da8d8302b623095e395688071b5e0b3925d
Shumon Huque <shuque@gmail.com>
2020-04-09 15:25:01 -0400

    Misc improvements.

--- validate1.py
+++ validate1.py
@@ -4,11 +4,19 @@
 """
 Testing DNSSEC signature validation.
 for details: RFC 4034, Section 3.1.8.1.  Signature Calculation
+
+Given a zone name, query its SOA RRset, and verify the SOA signature
+against the apex DNSKEY RRset.
+
 TODO:
      - Check signature validity time
+     - raise error if we don't have proper crypto library version
+     - test suite to excercise misc error conditions.
+     - generalized DNSSEC key dict for many names.
 """
 
 import sys
+import time
 import struct
 from io import BytesIO
 import dns.resolver
@@ -20,6 +28,9 @@ from Crypto.PublicKey import RSA, ECC
 from Crypto.Signature import pkcs1_15, DSS
 from Crypto.Hash import SHA1, SHA256, SHA384, SHA512
 
+
+# Tolerable clock skew for signatures in seconds
+CLOCK_SKEW = 300
 
 # DNSSEC algorithm -> hash function
 HASHFUNC = {
@@ -43,6 +54,17 @@ class DNSKEYinfo:
             self.key = keydata_to_rsa(rr.key)
         elif self.algorithm in [13, 14]:
             self.key = keydata_to_ecc(self.algorithm, rr.key)
+        else:
+            raise ValueError("DNSKEY algorithm {} not supported".format(
+                self.algorithm))
+
+
+class SIGinfo:
+
+    def __init__(self, data_rrset, sig_rrset):
+        self.data = data_rrset
+        self.sigs = sig_rrset
+        pass
 
 
 def get_resolver(dnssec_ok=False, timeout=5):
@@ -116,6 +138,8 @@ def get_sig_hashes(rrset, rrsigs):
     """
     For given rrset and rrsig set, calculate the signature hash to be
     verified for each signature in the rrsig set. Yield one at a time.
+    From RFC4034, this is: hash(RRSIG_RDATA | RR(1) | RR(2)... ), where
+    RRSIG_RDATA is the rdata minus the actual signature field.
     """
 
     rrname = rrset.name
@@ -140,7 +164,24 @@ def get_sig_hashes(rrset, rrsigs):
             rrdata = rr.to_digestable()
             rrlen = struct.pack('!H', len(rrdata))
             h.update(rrlen + rrdata)
-        yield h, signature
+        yield h, signature, sig_rdata
+
+
+def check_time(sig_rdata, skew=CLOCK_SKEW):
+    """
+    Check that current time is within signature validity period, plus
+    minus an acceptable clock skew.
+    """
+
+    current_time = int(time.time() + 0.5)
+    print("DEBUG: ", sig_rdata.expiration-current_time)
+    ok1 = (current_time >= sig_rdata.inception) or \
+        (abs(sig_rdata.inception - current_time) <= skew)
+    ok2 = (current_time <= sig_rdata.expiration) or \
+        (abs(current_time - sig_rdata.expiration) <= skew)
+    if ok1 and ok2:
+        return
+    raise ValueError("Error: Signature validity time is invalid")
 
 
 def validate(rrset, rrsigs, dnskey_list):
@@ -151,36 +192,25 @@ def validate(rrset, rrsigs, dnskey_list):
     """
 
     Verified = False
-
     verified_list = []                # list of (keytag, algo)
 
-    for h, signature in get_sig_hashes(rrset, rrsigs):
+    for h, signature, sig_rdata in get_sig_hashes(rrset, rrsigs):
 
         for keyinfo in DNSSEC_KEYS:
-            keyname = keyinfo.name
-            flags = keyinfo.flags
-            keytag = keyinfo.keytag
-            algo = keyinfo.algorithm
-            key = keyinfo.key
-
-            if isinstance(key, RSA.RsaKey):
-                verifier = pkcs1_15.new(key)
-                try:
-                    verifier.verify(h, signature)
-                except ValueError:
-                    pass
-                else:
-                    Verified = True
-                    verified_list.append((keytag, algo))
-            elif isinstance(key, ECC.EccKey):
-                verifier = DSS.new(key, 'fips-186-3')
-                try:
-                    verifier.verify(h, signature)
-                except ValueError:
-                    pass
-                else:
-                    Verified = True
-                    verified_list.append((keytag, algo))
+            if isinstance(keyinfo.key, RSA.RsaKey):
+                verifier = pkcs1_15.new(keyinfo.key)
+            elif isinstance(keyinfo.key, ECC.EccKey):
+                verifier = DSS.new(keyinfo.key, 'fips-186-3')
+            else:
+                raise ValueError("Unknown key type")
+            try:
+                verifier.verify(h, signature)
+                check_time(sig_rdata)
+            except ValueError:
+                pass
+            else:
+                Verified = True
+                verified_list.append(keyinfo)
 
     return Verified, verified_list
 
@@ -194,20 +224,27 @@ if __name__ == '__main__':
     r = get_resolver(dnssec_ok=True)
     soa_rrset, soa_rrsigs = get_rrset(r, qname, qtype)
     print(soa_rrset)
+    if soa_rrsigs is None:
+        print("No signatures found.")
+        sys.exit(1)
+
+    print("Found {} signature{}.".format(
+        len(soa_rrsigs), "" if len(soa_rrsigs) == 1 else "s"))
+    print('')
 
     dnskey_rrset, _ = get_rrset(r, qname, dns.rdatatype.from_text('DNSKEY'))
     DNSSEC_KEYS = load_keys(dnskey_rrset)
     for keyinfo in DNSSEC_KEYS:
         print("DNSKEY: {} {} {} {}".format(
             keyinfo.name, keyinfo.flags, keyinfo.keytag, keyinfo.algorithm))
+    print('')
 
     valid, valid_info = validate(soa_rrset, soa_rrsigs, DNSSEC_KEYS)
-
     if valid:
         print("OK: Signature Verified")
-        for keytag, algo in valid_info:
-            print("     Good signature with keytag={} algo={}".format(
-                keytag, algo))
+        for keyinfo in valid_info:
+            print("    Good signature with keytag={} algo={}".format(
+                keyinfo.keytag, keyinfo.algorithm))
         sys.exit(0)
     else:
         print("FAIL: No Signature Verified with any DNSKEY")
